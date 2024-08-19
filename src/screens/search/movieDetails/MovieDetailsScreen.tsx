b711ddff96ec0e8dc5ee26a90d2dee898459a8e5
Bartosz Bednarski <bambocha1997@gmail.com>
2024-08-20 01:13:49 +0200

    Added assets and got stuck with Video implementation.

--- src/screens/search/movieDetails/MovieDetailsScreen.tsx
+++ src/screens/search/movieDetails/MovieDetailsScreen.tsx
@@ -1,8 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
-
-const MovieDetailsScreen: React.FC = () => {
+import Video from "react-native-video";
+const MovieDetailsScreen = () => {
+  const background = require("../../../assets/video/broadchurch.mp4");
   return (
     <View style={styles.container}>
+      {/* <Video source={{ uri: "https://vjs.zencdn.net/v/oceans.mp4" }} /> */}
       <Text>Movie details screen</Text>
     </View>
   );
@@ -13,6 +15,13 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: "white",
     paddingHorizontal: 24,
+  },
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 export default MovieDetailsScreen;
